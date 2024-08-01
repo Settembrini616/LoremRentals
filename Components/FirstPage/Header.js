49263ff5a362dc76d91a8fff704d926ddf69e762
Ushumba <ipolovyi94@gmail.com>
2024-08-01 00:46:21 -0600

    August 1st commit

--- Components/FirstPage/Header.js
+++ Components/FirstPage/Header.js
@@ -27,10 +27,30 @@ function initializeHeader(){
            
         </div>
 
-            <div class="nav__right">
-            SOMETHING
+        <div class="nav__right">
+
+        <div class="nav__right--top">
+            
+            <div>&nbsp;</div>
+           
+            <ul class="nav__right--top-content">
+                <li class="nav__right--top-content_promo">promo</li>
+                <li class="nav__right--top-content_socialMedia">social</li>
+                <li class="nav__right--top-content_phone">+1(519)200-8736</li>
+                <li class="nav__right--top-content_search">Search</li>
+            </ul>
+        </div>
+
+        <div class="nav__right--bottom">
+            <ul class="nav__right--bottom-list">
+                <li class="nav__right--bottom-item_1">ABC</li>
+                <li class="nav__right--bottom-item_2">DEF</li>
+                <li class="nav__right--bottom-item_3">GHI</li>
+                <li class="nav__right--bottom-item_4">JKL</li>
+            </ul>
+        </div>
             </div>
-         </div>
+        </div>
         `;
 
         mainMenu.innerHTML = navbarHTML;
